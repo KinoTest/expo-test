4ddbeb6fb1056c3250a4f26efd783a0b9ba06e34
DanBaDo <bytes4life@proton.me>
2025-04-24 17:41:20 +0200

    Provides a put endpoint for Task
    Provides JSON parser middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import TasksController from "./controllers/TasksController.js";
+import bodyParser from "body-parser";
 
 interface OptionalIdParam {
   id?: string
 }
 
+const jsonMiddleware = bodyParser.json()
 const tasksController = new TasksController()
 
 // configures dotenv to work in your application
@@ -21,6 +22,12 @@ const PORT = process.env.PORT;
 app.get('/task/{:id}', async (request: Request<OptionalIdParam>, response: Response) => {
   const output = await tasksController.get(request.params.id)
   response.status(200).send(output)
+})
+
+app.put('/task/', jsonMiddleware, async (request: Request, response: Response) => {
+  console.log(request.body)
+  response.status(200).send(JSON.stringify(request.body))
+  //TODO: Update database row
 })
 
 app.listen(PORT, () => { 
