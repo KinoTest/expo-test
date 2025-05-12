787479d90e86c60d58b3b57835015597810abf66
DanBaDo <bytes4life@proton.me>
2025-05-12 11:43:56 +0200

    Styles example

--- frontend/app/Styles.tsx
+++ frontend/app/Styles.tsx
@@ -1,49 +1,74 @@
-import { StyleProp, StyleSheet, ViewStyle } from "react-native"
+import { StyleSheet } from "react-native"
+
+/**
+ * 
+ * Dispersion is reject on export, so we will use specific initialization for dispersion 
+ * before export.
+ * 
+ * Dispersion is necesary becouse a weird behavior on StyleSheet.create including some
+ * some properties like flex or flexDirection.
+ * 
+ * defaultStyles implements theme constants.
+ * 
+ */
 
 const defaultMarginValue = 10
 const defaultPaddingValue = 10
 
-const { defaultMargins } = StyleSheet.create ({
+const defaultsStyles = StyleSheet.create ({
     defaultMargins: {
-        margin: defaultMarginValue
-    }
-})
-
-const { defaultPaddings } = StyleSheet.create ({
+        margin: defaultMarginValue,
+    },
     defaultPaddings: {
-        margin: defaultPaddingValue
+        padding: defaultPaddingValue,
+    },
+    defaultSpacing: {
+        margin: defaultMarginValue,
+        padding: defaultPaddingValue,
     }
 })
 
-const { container } = StyleSheet.create ({
+const {
+    defaultMargins,
+    defaultPaddings,
+    defaultSpacing,
+} = defaultsStyles
+
+const componentsStyles = StyleSheet.create ({
     container: {
         backgroundColor: 'white',
         borderRadius: 10,
         shadowRadius: 5,
         shadowColor: 'rgb(180,180,180)',
-        ...defaultMargins,
-        ...defaultPaddings
-    }
-})
-
-const { cardComponent } = StyleSheet.create({
+        ...defaultSpacing,
+    },
     cardComponent: {
-        ...defaultMargins
-    }
-} ) 
-
-const cosa = StyleSheet.create({
-    switchComponent: {
-        flex: 1,
-        flexDirection: "row"
-    }
+        ...defaultMargins,
+    },
+    switchComponentStyle: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    taskComponentStyle: {
+        flexDirection: "column",
+        ...defaultSpacing,
+    },
 })
 
-const { switchComponent } = cosa // TODO: What the fuking fuck!!!
+const {
+    cardComponent,
+    container,
+    switchComponentStyle,
+    taskComponentStyle,
+} = componentsStyles
+
 
 export {
     defaultMargins,
+    defaultPaddings,
+    defaultSpacing,
     container,
     cardComponent,
-    switchComponent
+    switchComponentStyle,
+    taskComponentStyle,
 }
--- frontend/app/components/SwitchComponent.tsx
+++ frontend/app/components/SwitchComponent.tsx
@@ -1,12 +1,22 @@
 import { Switch, Text, View} from "react-native";
-import { switchComponent, defaultMargins } from "../Styles";
+import { switchComponentStyle, defaultMargins } from "../Styles";
 
-export default function SwitchComponent (props: {description: string, onToggle: EventListener }) {
+export default function SwitchComponent (props: {description: string, checked: boolean, onToggle: (value: boolean)=>void | Promise<void> }) {
 
-    return <div style={switchComponent}>
-        <Switch style={defaultMargins}/>
+    return <View style={switchComponentStyle}>
+
+        <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={props.checked ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={props.onToggle}
+            value={props.checked}
+            style={defaultMargins}
+        />
+
         <Text>{props.description}</Text>
-    </div>
+        
+    </View>
 
 }
 
